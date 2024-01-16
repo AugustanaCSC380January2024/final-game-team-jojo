33f6ec68ea1f608372c6f30313490d2b09b63694
Bill Unyi <unyi8799@gmail.com>
2024-01-16 17:06:25 -0600

    Added more animations and a gun particle effect

---
+++ scripts/level_1.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var respawnTimer = $RespawnTimer
+
+func _on_death_zone_body_entered(body):
+	body.die()
+	await get_tree().create_timer(1.5).timeout
+	get_tree().reload_current_scene()
+	body.respawn()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,48 +1,59 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var shoot_particles = preload("res://scenes/shoot_particles.tscn")
 
 @export var acceleration = 2000
 @export var max_speed = 500
 @export var jump_height = 600
 @export var gravity_strength = 980
 @export var friction = 1500
+var alive = true
+var attackShoot = false
+var attackMelee = false
 
 func _physics_process(delta):
-	if is_on_floor() == false:
-		if Input.is_action_just_pressed("melee"):
-			animated_sprite.play("jump_melee")
-		elif Input.is_action_just_pressed("shoot"):
-			animated_sprite.play("jump_gun")
-		else:
-			animated_sprite.play("jump")
+	if !is_on_floor():
+		#if alive:
+			#if Input.is_action_pressed("melee"):
+				#animated_sprite.play("jump_melee")
+			#elif Input.is_action_pressed("shoot"):
+				#animated_sprite.play("jump_gun")
+			#else:
+				#animated_sprite.play("jump")
 		velocity.y += gravity_strength * delta
 		if velocity.y > 1000:
 			velocity.y = 1000
-	if is_on_floor() == true:
-		if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
-			if Input.is_action_just_pressed("melee"):
-				animated_sprite.play("run_melee")
-			elif Input.is_action_just_pressed("shoot"):
-				animated_sprite.play("run_n_gun")
-			else:
-				animated_sprite.play("run")
-		else:
-			if Input.is_action_just_pressed("melee"):
-				animated_sprite.play("melee")
-			elif Input.is_action_just_pressed("shoot"):
-				animated_sprite.play("gun")
-			else:
-				animated_sprite.play("idle")
+	
+	#if is_on_floor() && alive:
+		#if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
+			#if Input.is_action_pressed("melee"):
+				#animated_sprite.play("run_melee")
+			#elif Input.is_action_pressed("shoot"):
+				#animated_sprite.play("run_n_gun")
+			#else:
+				#animated_sprite.play("run")
+		#else:
+			#if Input.is_action_pressed("melee"):
+				#animated_sprite.play("melee")
+			#elif Input.is_action_pressed("shoot"):
+				#animated_sprite.play("gun")
+			#else:
+				#animated_sprite.play("idle")
 	
 	if Input.is_action_just_pressed("jump") && is_on_floor()==true:
-			jump(jump_height)
-			
+		jump(jump_height)
 	if Input.is_action_just_pressed("move_down") && is_on_floor()==false:
-			velocity.y += 500
+		velocity.y += 500
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	if Input.is_action_just_pressed("melee"):
+		melee()
+	idle()
 	
 	var facing = 0
-	facing = Input.get_axis("move_left","move_right")
+	if alive:
+		facing = Input.get_axis("move_left","move_right")
 	if facing != 0:
 		animated_sprite.flip_h = (facing == -1)
 		velocity.x += facing * acceleration * delta
@@ -65,3 +76,53 @@ func _physics_process(delta):
 
 func jump(jump_height):
 	velocity.y = -jump_height
+
+func idle():
+	if alive && !attackMelee && !attackShoot:
+		if is_on_floor():
+			if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
+				animated_sprite.play("run")
+			else:
+				animated_sprite.play("idle")
+		else:
+			animated_sprite.play("jump")
+
+func melee():
+	if alive && !attackShoot:
+		attackMelee = true
+		if is_on_floor():
+			if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
+				animated_sprite.play("run_melee")
+			else:
+				animated_sprite.play("melee")
+		else:
+			animated_sprite.play("jump_melee")
+		await get_tree().create_timer(0.3).timeout
+		attackMelee = false
+		
+func shoot():
+	if alive && !attackMelee:
+		attackShoot = true
+		if is_on_floor():
+			if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
+				animated_sprite.play("run_n_gun")
+			else:
+				animated_sprite.play("gun")
+		else:
+			animated_sprite.play("jump_gun")
+			
+		var shot = shoot_particles.instantiate()
+		add_child(shot)
+		await get_tree().create_timer(0.216).timeout
+		shot.emitting = true
+		await get_tree().create_timer(0.3).timeout
+		shot.emitting = false
+		shot.queue_free()
+		attackShoot = false
+	
+func die():
+	alive = false
+	animated_sprite.play("die")
+
+func respawn():
+	alive = true
