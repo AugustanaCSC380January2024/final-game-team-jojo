ba0d3b0d604371f491920dd164165990cd715f35
John <johnwarhank21@augustana.edu>
2024-01-17 01:19:29 -0600

    Enemy now follows player upon entering sight radius

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,11 +2,26 @@ extends CharacterBody2D
 
 @onready var hitbox = preload("res://scenes/hitbox.tscn")
 @onready var animated_sprite = $AnimatedSprite2D
+@export var gravity_strength = 500
 var following = false
+var player = null
 
 func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity_strength * delta
+		if velocity.y > 1000:
+			velocity.y = 1000
 	if following:
-		
+		print(player.global_position.x)
+		if global_position.x - player.global_position.x > 20:
+			animated_sprite.flip_h = -1
+			velocity.x = -50
+			move_and_slide()
+		elif global_position.x - player.global_position.x < -20:
+			animated_sprite.flip_h = 1
+			velocity.x = 50
+			move_and_slide()
+		else: attack()
 
 func attack():
 	animated_sprite.play("attack")
@@ -18,5 +33,8 @@ func attack():
 
 
 func _on_sight_radius_body_entered(body):
-	if body is Player:
-		follow = true
+	print("entered")
+	if body.is_in_group("Player"):
+		following = true
+		player = body
+		print("player entered")
