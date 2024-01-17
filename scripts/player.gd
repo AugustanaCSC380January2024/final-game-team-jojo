478a3a57b46013d809870c0139c93835e9bfc66e
Warhank <johnwarhank21@augustana.edu>
2024-01-17 13:55:24 -0600

    Gun particles shoot in the correct direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var canvas_layer = $CanvasLayer
 @onready var shoot_particles = preload("res://scenes/shoot_particles.tscn")
 
 @export var acceleration = 2000
@@ -16,33 +17,9 @@ var currently_facing = 1
 
 func _physics_process(delta):
 	if !is_on_floor():
-		#if alive:
-			#if Input.is_action_pressed("melee"):
-				#animated_sprite.play("jump_melee")
-			#elif Input.is_action_pressed("shoot"):
-				#animated_sprite.play("jump_gun")
-			#else:
-				#animated_sprite.play("jump")
 		velocity.y += gravity_strength * delta
 		if velocity.y > 1000:
 			velocity.y = 1000
-	
-	#if is_on_floor() && alive:
-		#if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
-			#if Input.is_action_pressed("melee"):
-				#animated_sprite.play("run_melee")
-			#elif Input.is_action_pressed("shoot"):
-				#animated_sprite.play("run_n_gun")
-			#else:
-				#animated_sprite.play("run")
-		#else:
-			#if Input.is_action_pressed("melee"):
-				#animated_sprite.play("melee")
-			#elif Input.is_action_pressed("shoot"):
-				#animated_sprite.play("gun")
-			#else:
-				#animated_sprite.play("idle")
-	
 	if Input.is_action_just_pressed("jump") && is_on_floor()==true && alive:
 		jump(jump_height)
 	if Input.is_action_just_pressed("move_down") && is_on_floor()==false:
@@ -73,6 +50,7 @@ func _physics_process(delta):
 		velocity.x = max_speed
 	elif velocity.x < -max_speed:
 		velocity.x = -max_speed
+	flip()
 	
 	move_and_slide()
 
@@ -114,14 +92,19 @@ func shoot():
 			animated_sprite.play("jump_gun")
 			
 		var shot = shoot_particles.instantiate()
-		add_child(shot)
-		shot.face_shot(shot, currently_facing)
+		canvas_layer.add_child(shot)
 		await get_tree().create_timer(0.216).timeout
 		shot.emitting = true
 		await get_tree().create_timer(.5).timeout
 		shot.emitting = false
 		shot.queue_free()
 		attackShoot = false
+		
+func flip():
+	if currently_facing == 1:
+		canvas_layer.rotation = 0
+	else:
+		canvas_layer.rotation = 3.14
 	
 func die():
 	alive = false
--- scripts/shoot_particles.gd
+++
@@ -1,6 +0,0 @@
-extends GPUParticles2D
-
-func face_shot(shot, direction):
-	if direction == -1 or 0:
-		shot.position = Vector2(-29, 9)
-		shot.scale = Vector2(-1,1)
