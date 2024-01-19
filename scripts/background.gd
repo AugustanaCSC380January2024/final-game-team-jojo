fddd6a8931b4b8dfb4eb6075675e541016d46ac0
Bill Unyi <unyi8799@gmail.com>
2024-01-19 05:27:59 -0600

    More Background Effects

--- scripts/background.gd
+++ scripts/background.gd
@@ -3,8 +3,10 @@ extends ParallaxBackground
 @onready var sky = $SkyLayer
 @onready var ocean = $OceanLayer
 @onready var sand = $SandLayer
+var time = 0
 
 func _process(delta):
+	time += delta
 	sand.motion_offset = Vector2(GlobalValues.playerPosition.x / 16, GlobalValues.playerPosition.y / 64)
-	ocean.motion_offset = Vector2(GlobalValues.playerPosition.x / 4, GlobalValues.playerPosition.y / 16)
+	ocean.motion_offset = Vector2(GlobalValues.playerPosition.x / 4 - sin(time/3) * 60, GlobalValues.playerPosition.y / 16 + cos(time/4) * 15)
 	sky.motion_offset = Vector2(GlobalValues.playerPosition.x / 2, GlobalValues.playerPosition.y / 8)
