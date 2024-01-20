3cafbefadb8e76ba3e8f33281dc4dab11d627f47
John <johnwarhank21@augustana.edu>
2024-01-20 11:27:15 -0600

    Fixed cannon enemy, changed player camera y limit

--- scripts/cannonball.gd
+++ scripts/cannonball.gd
@@ -7,14 +7,15 @@ var player_location = null
 var exploded = false
 var angle = 0
 
-func _on_ready():
+func _ready():
 	player_location = GlobalValues.playerPosition
 	var deltaDistance = player_location - global_position
 	angle = atan2(deltaDistance.y, deltaDistance.x)
+	print((angle/(2 * PI)) * 360)
 
 func _process(delta):
-	global_position.x += sin(angle) * cannon_speed * delta
-	global_position.y += cos(angle) * cannon_speed * delta
+	global_position.x += cos(angle) * cannon_speed * delta
+	global_position.y += sin(angle) * cannon_speed * delta
 
 func _on_body_entered(body):
 	if body.is_in_group("Player") && !exploded:
