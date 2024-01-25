69d59f38e637d5163c3501b28dcf998e3c7928f5
John <johnwarhank21@augustana.edu>
2024-01-25 17:44:27 -0600

    Credits working

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
+@onready var image = $CanvasLayer/TextureRect
+@onready var credits = load("res://assets/credits.png")
+
 func _ready():
 	GlobalValues.player.hud.visible = false
+
+func _on_button_pressed():
+	image.set_texture(credits)
